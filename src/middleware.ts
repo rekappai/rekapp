17500d09829ba9c4f9ee69264cfd2bb12fafdd9f
rekappai <info@rekapp.ai>
2026-03-25 10:55:26 +0000

    Fix middleware matcher to exclude API routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -34,5 +34,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next|api|favicon.ico).*)'],
+  matcher: ['/((?!_next|api|favicon.ico|.*\\..*).*)'  ],
 }
