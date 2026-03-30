d37d793f773393c22d4a991edc56c497e0fb9c47
rekappai <info@rekapp.ai>
2026-03-30 03:02:39 +0000

    fix: add rewrites to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      { source: '/robots.txt', destination: '/api/robots' },
+      { source: '/sitemap.xml', destination: '/api/sitemap' },
+    ]
+  },
 };
-
 export default nextConfig;
